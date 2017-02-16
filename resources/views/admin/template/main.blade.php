d0ed90501efe1d69a50d3666e99d7ce8bfdf2eca
Juan David Vallejo Restrepo <jdvallejor@unal.edu.co>
2017-02-16 08:21:00 -0500

    tutorial 11

--- resources/views/admin/template/main.blade.php
+++ resources/views/admin/template/main.blade.php
@@ -6,8 +6,15 @@
 	<link rel="stylesheet" href="{{ asset('plugins/bootstrap/css/bootstrap.css') }}">
 </head>
 <body>
+
+	@include('admin.template.partials.nav')
+
 	<section>
 		@yield('content')
 	</section>
+
+	<script src="{{ asset('plugins/jquery/js/jquery.min.js') }}"></script>
+	<script src="{{ asset('plugins/bootstrap/js/bootstrap.js') }}"></script>
+
 </body>
 </html>
